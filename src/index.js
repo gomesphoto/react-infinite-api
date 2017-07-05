d80db584981a9e7c058d5214522ec23e310c333d
Pedro Gomes <pedro@loot.io>
2017-07-05 18:18:37 +0100

    fix watcherRegister

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom';
-import React, { Component } from 'react'
-import styled from 'styled-components'
-import scrollMonitor from 'scrollmonitor'
-import { loremApi, parseData } from './helpers'
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import scrollMonitor from 'scrollmonitor';
+import { loremApi, parseData } from './helpers';
 
 const StyledList = styled.ul`
   text-align: center;
@@ -14,74 +14,50 @@ const StyledList = styled.ul`
   border: 1px solid black;
   list-style: none;
   font-family: sans-serif;
-`
+`;
 
 const StyledItem = styled.li`
   font-size: 30px;
   border-bottom: 1px solid black;
-`
-let containerMonitor
+`;
 
-const bindContainerMonitor = () => {
-  const containerElement = document.getElementById('listContainer')
-  containerMonitor = scrollMonitor.createContainer(containerElement)
-  return containerMonitor
-}
-
-let watcherTargetIndex = 24;
-let watcherRegister = null
+let containerMonitor;
+let containerElement;
+let watcherElement;
+let watcherRegister = null;
 
 class App extends Component {
   state = {
-    items: [],
-    hasMore: true
+    items: []
   }
 
   componentDidMount = () => {
-    bindContainerMonitor()
-    this.callApi()
+    containerMonitor = scrollMonitor.createContainer(containerElement);
+    this.apiTrigger();
   }
 
-  callApi = () =>
-    loremApi(30).then(({ data }) => this.loadMore(data)).then(this.applyScrollMonitor)
-
-  loadMore = (data) => {
-    const moreItems = parseData(data)
-    if (this.state.items > 100) {
-      this.setState({ hasMore: false })
-    } else {
+  apiTrigger = () =>
+    loremApi(30)
+    .then(({ data }) => {
+      const moreItems = parseData(data)
       const items = [ ...this.state.items, ...moreItems ];
       this.setState({ items })
-    }
-  }
-
-  onEnterViewport = () => {
-    this.callApi()
-    watcherRegister.destroy()
-  }
-
-  applyScrollMonitor = () => {
-    var listElements = document.getElementsByClassName('list-item')
-
-    console.log('APPLYSCROLLMONITOR / addEventListener to index', watcherTargetIndex)
-    const watcher = containerMonitor.create(listElements[watcherTargetIndex])
-    watcher.enterViewport(this.onEnterViewport)
-    watcherTargetIndex += 25
-    watcherRegister = watcher
-    }
-
-  renderItems = items =>
-    items.map((item, idx) => (
-      <StyledItem key={`lorem-${idx}`} className="list-item">{`${idx} ${item}`}</StyledItem>
-    ))
+    })
+    .then(() => {
+      if (watcherRegister) {
+        watcherRegister.destroy()
+      }
+    })
+    .then(() => {
+      watcherRegister = containerMonitor.create(watcherElement)
+      watcherRegister.enterViewport(this.apiTrigger);
+    });
 
   render = () => (
-    <div>
-      <StyledList id="listContainer">
-        {this.renderItems(this.state.items)}
+      <StyledList innerRef={x => containerElement = x}>
+        {this.state.items.map((item, idx) => (<StyledItem key={`lorem-${idx}`} innerRef={x => (idx === this.state.items.length - 6) ? watcherElement = x : null}>{`${idx} ${item}`}</StyledItem>))}
       </StyledList>
-    </div>
-  )
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
